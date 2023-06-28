a27fecf27dfe60d47f1924d3fd399e17b81e7b70
canzve <canzve@icloud.com>
2023-06-28 22:38:57 +0200

    Initial Commit

---
+++ Memory/MemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryApp.swift
+//  Memory
+//
+//  Created by Nebojsa Pavlovic on 28.6.23..
+//
+
+import SwiftUI
+
+@main
+struct MemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
