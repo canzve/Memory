68c378e30566a5dbba7dc8bb759f81f86b7fc26b
canzve <canzve@icloud.com>
2023-06-28 22:49:59 +0200

    first commit

--- Memory/MemoryApp.swift
+++ Memory/MemoryApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct MemoryApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
